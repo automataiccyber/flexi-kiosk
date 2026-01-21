eaa9659fe37dbee0c6cf5bf72056bd06050a165b
Liel Darren Fajutagana <lieldarrenfajutagana@gmail.com>
2026-01-21 18:22:46 +0800

    Configure Firebase web app credentials

--- firebase-config.js
+++ firebase-config.js
@@ -1,8 +1,8 @@
 window.FIREBASE_CONFIG = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+  apiKey: "AIzaSyAHs-Pjs9ZbF09mr83HKK1C8YAkLErV5d8",
+  authDomain: "flexi-kiosk.firebaseapp.com",
+  projectId: "flexi-kiosk",
+  storageBucket: "flexi-kiosk.firebasestorage.app",
+  messagingSenderId: "448729015169",
+  appId: "1:448729015169:web:534fddd23a2fe0cae9fe80"
 };
